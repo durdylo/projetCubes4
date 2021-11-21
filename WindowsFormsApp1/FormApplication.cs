03c12b24f91b3f62627cdbad707584c9cfb9cb20
Durdylo <loic.albert.denis@gmail.com>
2021-11-21 16:08:55 +0100

    app

--- WindowsFormsApp1/FormApplication.cs
+++ WindowsFormsApp1/FormApplication.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1.Presentation.GestionSites;
 
 namespace WindowsFormsApp1
 {
@@ -15,6 +16,12 @@ namespace WindowsFormsApp1
         public FormApplicaation()
         {
             InitializeComponent();
+        }
+
+        private void gérerLesSitesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormGererSites form = new FormGererSites();
+            form.Show();
         }
     }
 }
